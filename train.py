ac22f38a562f80a7c1f1a80f8266545087806017
lzh109 <1091038505@qq.com>
2021-01-17 13:43:31 +0800

    code without artificial feature

--- train.py
+++ train.py
@@ -6,6 +6,8 @@ import os
 import datetime
 from sklearn.metrics import f1_score,auc,roc_curve,accuracy_score,recall_score,precision_score,roc_auc_score
 
+datapath="G:/lzhLaser/laserFilm/data"
+
 #得到当前时间
 def get_time():
     time= datetime.datetime.now()
@@ -42,10 +44,17 @@ def readimage(path,label):
                 if f.split(".")[-1] == 'jpg':
                     image = cv2.imread(os.path.join(root, f))
                     image=image/255
+                    image=image.reshape(3,224,224)
                     X.append(image)
                     Y.append(label[p])
             p+=1
-    return np.array(X),np.array(Y)
+    X=np.array(X)
+    Y=np.array(Y)
+    state = np.random.get_state()
+    np.random.shuffle(X)
+    np.random.set_state(state)
+    np.random.shuffle(Y)
+    return X,Y
 
 
 #打印当前模型的每层冻结情况,包括thresh
@@ -61,13 +70,13 @@ def printModel(model,update,thresh):
                 print(i, layer.name, 'trainable:True')
 #返回改进的vgg19网络模型
 def vgg19(update=False):
-    vgg19=tf.keras.applications.vgg19.VGG19(weights='imagenet',input_shape=(224,224,3),include_top=False)
+    vgg19=tf.keras.applications.vgg19.VGG19(weights='imagenet', input_shape=(3,224, 224), include_top=False)
     x = vgg19.output
-    x = tf.keras.layers.Flatten(name='flatten')(x)
+    x = tf.keras.layers.GlobalAveragePooling2D()(x)
     x = tf.keras.layers.Dense(4096, activation='relu', name='fc1')(x)
     x = tf.keras.layers.Dense(1024, activation='relu', name='fc2')(x)
     x = tf.keras.layers.Dense(1, activation='sigmoid', name='predictions')(x)
-    myvgg=tf.keras.models.Model(inputs=vgg19.inputs,outputs=x)
+    myvgg=tf.keras.models.Model(inputs=vgg19.inputs, outputs=x)
     thresh=-1
     if update == False:
         thresh=21
@@ -75,10 +84,11 @@ def vgg19(update=False):
     return myvgg
 
 def iv3(update=False):
-    iv3 = tf.keras.applications.inception_v3.InceptionV3(weights='imagenet', input_shape=(224, 224, 3), include_top=False)
+    iv3 = tf.keras.applications.inception_v3.InceptionV3(weights='imagenet', input_shape=(3,224, 224), include_top=False)
     x = iv3.output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(1024, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(4096, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(1024, activation='relu', name='fc2')(x)
     x = tf.keras.layers.Dense(1, activation='sigmoid', name='predictions')(x)
     myiv3 = tf.keras.models.Model(inputs=iv3.inputs, outputs=x)
     thresh = -1
@@ -87,11 +97,12 @@ def iv3(update=False):
     printModel(myiv3, update, thresh)
     return myiv3
 
-def densenet(update=False):
-    densenet = tf.keras.applications.densenet.DenseNet121(weights='imagenet', input_shape=(224, 224, 3),include_top=False)
-    x = iv3.output
+def densenet121(update=False):
+    densenet = tf.keras.applications.densenet.DenseNet121(weights='imagenet', input_shape=(3,224, 224),include_top=False)
+    x = densenet.output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(1024, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(4096, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(1024, activation='relu', name='fc2')(x)
     x = tf.keras.layers.Dense(1, activation='sigmoid', name='predictions')(x)
     mydense = tf.keras.models.Model(inputs=densenet.inputs, outputs=x)
     thresh = -1
@@ -100,30 +111,20 @@ def densenet(update=False):
     printModel(mydense, update, thresh)
     return mydense
 
-def resnet(update=False):
-    resnet = tf.keras.applications.resnet50.ResNet50(weights='imagenet', input_shape=(224, 224, 3),include_top=False)
+def resnet50(update=False):
+    resnet = tf.keras.applications.resnet50.ResNet50(weights='imagenet', input_shape=(3,224, 224),include_top=False)
     x = resnet.output
     x = tf.keras.layers.GlobalAveragePooling2D()(x)
-    x = tf.keras.layers.Dense(1024, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(4096, activation='relu', name='fc1')(x)
+    x = tf.keras.layers.Dense(1024, activation='relu', name='fc2')(x)
     x = tf.keras.layers.Dense(1, activation='sigmoid', name='predictions')(x)
-    myres = tf.keras.models.Model(inputs=densenet.inputs, outputs=x)
+    myres = tf.keras.models.Model(inputs=resnet.inputs, outputs=x)
     thresh = -1
     if update == False:
         thresh = len(resnet.layers)-1
     printModel(myres, update, thresh)
     return myres
 
-
-
-def kf():
-    X = np.array([[1, 2], [3, 4], [1, 2], [3, 4],[2,2],[2,3]])
-    y = np.array([1, 2, 2, 1, 2, 2])
-
-    kf = KFold(n_splits=2)
-    for train_index, test_index in kf.split(X):
-        print('train_index', train_index, 'test_index', test_index)
-        train_X, train_y = X[train_index], y[train_index]
-        test_X, test_y = X[test_index], y[test_index]
 
 
 #epoch,train_loss,vali_loss,test_loss,train_acc,vali_acc,test_acc,test_auc,test_f1,test_recall,test_precision
@@ -160,11 +161,12 @@ def warpMycall(mycall):
     return data
 
 class mycallback(tf.keras.callbacks.Callback):
-    def __init__(self,test_x,test_y,weights_savedir):
+    def __init__(self,test_x,test_y,weights_savedir,kf):
         self.test_feature=test_x
         self.test_y=test_y
         self.weights_savedir=weights_savedir
         self.epoch_begin_time=datetime.datetime.now()
+        self.kf=kf
     def on_train_begin(self, logs={}):
         self.epoch=[]
         self.train_loss=[]
@@ -186,8 +188,9 @@ class mycallback(tf.keras.callbacks.Callback):
     def on_epoch_end(self, epoch, logs={}):
         cur=datetime.datetime.now()
         dur=cur-self.epoch_begin_time
-        self.train_time.append(dur.seconds)
-        self.model.save_weights(self.weights_savedir+"/"+"epoch-"+("%d"%epoch)+".h5")
+        self.train_time.append(dur.microseconds)
+        if epoch>35:
+            self.model.save_weights(self.weights_savedir+"/"+"kf-"+str(self.kf)+"epoch-"+("%d"%epoch)+".h5")
         self.epoch.append(epoch)
         self.train_acc.append(logs.get('binary_accuracy'))
         self.train_loss.append(logs.get('loss'))
@@ -197,7 +200,7 @@ class mycallback(tf.keras.callbacks.Callback):
         test_y_pred=self.model.predict(self.test_feature)
         end=datetime.datetime.now()
         during=end-begin
-        self.test_time.append(during.seconds)
+        self.test_time.append(during.microseconds)
         test_y_pred=test_y_pred.reshape(-1)
         test_acc=tf.keras.metrics.binary_accuracy(self.test_y,test_y_pred)
         test_loss=tf.keras.losses.binary_crossentropy(self.test_y,test_y_pred)
@@ -216,28 +219,57 @@ class mycallback(tf.keras.callbacks.Callback):
         self.test_f1.append(f1_score(self.test_y,test_y_pre))
         self.test_precision.append(precision_score(self.test_y,test_y_pre))
         self.test_recall.append(recall_score(self.test_y,test_y_pre))
-def train(model,X,Y,weights_save_dir,data_save_dir,epoch):
-    model.compile(loss=tf.keras.losses.binary_crossentropy,
-                  optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
-                  metrics=[tf.keras.metrics.binary_accuracy])
-    kf = StratifiedKFold(n_splits=5)
-    p=1
-    for train_index, test_index in kf.split(X,Y):
-        title="number: "+str(p)+" fold"
-        train_x, vali_x,train_y,vali_y= train_test_split(X[train_index],Y[train_index],test_size=0.15,random_state=0)
-        testX,testY= X[test_index], Y[test_index]
-        mycall=mycallback(testX,testY,weights_save_dir)
-        model.fit(train_x,train_y,epochs=epoch,shuffle=True,validation_data=(vali_x,vali_y),callbacks=[mycall])
-        data=warpMycall(mycall)
-        saveDataToTxt(data_save_dir,title,data)
-        p+=1
 
-def vgg_train():
-    X,Y=readimage('/Users/lizhenhao/Desktop/laserfilm/resize',[0,1])
-    vgg=vgg19(update=False)
-    train(vgg,X,Y,'weights/freeze/vgg','data/freeze/vgg.txt',1)
+
+
+def switchModel(model_name,update=False):
+    if model_name == 'vgg19':
+        return vgg19(update)
+    elif model_name=='resnet50':
+        return resnet50(update)
+    elif model_name=='densenet121':
+        return densenet121(update)
+    elif model_name=='iv3':
+        return iv3(update)
+    else:
+        raise ValueError("no model")
+def train(model_name,X,Y,weights_save_dir,data_save_dir,epoch):
+    kf = StratifiedKFold(n_splits=5,random_state=0)
+    p=1
+    for i in range(2):
+        if i==0:
+            update=False
+            wsd=weights_save_dir+'/freeze/'+model_name
+            dsp=data_save_dir+'/freeze/'+model_name+'.txt'
+        else:
+            update=True
+            wsd=weights_save_dir+'/update/'+model_name
+            dsp=data_save_dir+'/update/'+model_name+'.txt'
+        for train_index, test_index in kf.split(X,Y):
+            model=switchModel(model_name,update)
+            model.summary()
+            model.compile(loss=tf.keras.losses.binary_crossentropy,
+                          optimizer=tf.keras.optimizers.Adam(learning_rate=0.001),
+                          metrics=[tf.keras.metrics.binary_accuracy])
+            title="add fc layer "+"number: "+str(p)+" fold"
+            print(title)
+            train_x, vali_x,train_y,vali_y= train_test_split(X[train_index],Y[train_index],test_size=0.15,random_state=0)
+            testX, testY = X[test_index], Y[test_index]
+            mycall=mycallback(testX,testY,wsd,p)
+            model.fit(train_x,train_y,epochs=epoch,shuffle=True,validation_data=(vali_x, vali_y), callbacks=[mycall])
+            data=warpMycall(mycall)
+            saveDataToTxt(dsp,title,data)
+            p+=1
+
+def model_train(modelname):
+    X,Y=readimage(datapath,[0,1])
+    train(modelname,X,Y,'weights','data',40)
 
 if __name__=='__main__':
-    vgg_train()
+    model_train('vgg19')
+    model_train('densenet121')
+    model_train('resnet50')
+    model_train('iv3')
+
 
 
